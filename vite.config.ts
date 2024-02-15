4870e8d750919dded6389ac4203f2b0fc092caf9
muzaffar640 <muzaffar640@gmail.com>
2024-02-16 00:12:07 +0530

    add axios, react-query and react-toastify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import envCompatible from "vite-plugin-env-compatible";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), reactRefresh(), envCompatible({ prefix: "REACT_APP_" })],
+});
