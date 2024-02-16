13fd3069c69dcdedbb653efb9778e530ace00d45
muzaffar640 <muzaffar640@gmail.com>
2024-02-16 09:30:43 +0530

    add Add button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,11 +7,25 @@ import { useEmployeeList } from "../api/EmployeeList";
 import { Loader } from "../components/Common/Loader";
 import { styled } from "@mui/material/styles";
 import Toaster from "../components/Common/Toaster";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
 
 const StyledDashboard = styled("div")({
   padding: "64px 60px",
   backgroundColor: "#f9f9fb",
 });
+const AddButton = styled(Fab)(({ theme }) => ({
+  position: "fixed",
+  zIndex: 1000,
+  [theme.breakpoints.up("xs")]: {
+    bottom: "40px",
+    right: "40px",
+    [theme.breakpoints.up("sm")]: {
+      bottom: "60px",
+      right: "60px",
+    },
+  },
+}));
 
 const Dashboard = () => {
   const { filter } = useFilterContext();
@@ -39,6 +53,9 @@ const Dashboard = () => {
         <Administrators administrators={administrators} />
         <Divider sx={{ marginTop: "50px", marginBottom: "20px" }} />
         <Members members={members} />
+        <AddButton color="primary" aria-label="add">
+          <AddIcon color="info" fontSize="large" />
+        </AddButton>
       </StyledDashboard>
     </>
   );
